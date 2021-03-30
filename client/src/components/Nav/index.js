d2e54be30d804f9e499f2036e9af10b87e9c5fba
MNM <moussanm2014@gmail.com>
2021-03-29 22:07:45 -0500

    modified client/src/components/Nav/index.js file

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function Nav() {
+function Nav({children}) {
   return (
+    <div style={{ height: 70, margin: "2px" }}> {children}
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         Google Book Search
@@ -9,10 +10,11 @@ function Nav() {
       <a className="navbar-brand" href="/search">
         Search
       </a>
-      <a className="navbar-brand" href="/saved">
-        Saved
+      <a className="navbar-brand" href="/Saved">
+        Saved Books
       </a>
     </nav>
+     </div>
   );
 }
 
