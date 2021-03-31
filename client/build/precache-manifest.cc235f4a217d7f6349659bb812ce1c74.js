aaaab69a661529c41314ee59859b0591628b9d0d
MNM <moussanm2014@gmail.com>
2021-03-30 22:19:22 -0500

    npm run build created client/build/ folder

---
+++ client/build/precache-manifest.cc235f4a217d7f6349659bb812ce1c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ac0d6742f108e07424ff16d5f1d9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676ded4993f418a89fb5",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "e38011335481e1f6c295",
+    "url": "/static/css/main.74295e4a.chunk.css"
+  },
+  {
+    "revision": "676ded4993f418a89fb5",
+    "url": "/static/js/2.e90b58fe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e90b58fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38011335481e1f6c295",
+    "url": "/static/js/main.f7451fdd.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
