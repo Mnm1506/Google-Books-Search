be2dd981a5174ad232bec36cb53e43bbe28d6ff4
MNM <moussanm2014@gmail.com>
2021-03-30 22:21:33 -0500

    modified client/src/components/ViewBtn/index.js file

--- client/src/components/ViewBtn/index.js
+++ client/src/components/ViewBtn/index.js
@@ -1,8 +1,8 @@
 import React from "react" 
 
-function Button(props) {
+function ViewBtn(props) {
 
     return (<button style={{ float: "right", marginBottom: 10 }} className="btn btn-success" onClick={() => window.location.href=props.link} formTarget="_blank">View</button>)
 }
 
-export default Button;
+export default ViewBtn;
